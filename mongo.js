991155a40c40fd48fb2d4adbac28a038a4e4a3d5
DaniFL <danielfernandezlopez75@gmail.com>
2024-10-06 20:55:11 +0200

    Integración de NodeJs + dependencias + mongodb + nodemon + conf Mongo-node + corrección .gitignore

---
+++ mongo.js
@@ -0,0 +1,24 @@
+const {MongoClient} = require('mongodb');
+
+async function main() {
+    const uri = "mongodb+srv://root:root@dogmunity.buz48.mongodb.net/?retryWrites=true&w=majority&appName=Dogmunity";
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        await listDatabases(client);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+//Listas bases de datos del cluster de mongo
+async function listDatabases(client) {
+    const databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
