63bb460d1bd6edb7441894bba841cf6f1c732317
Inés del Río <ines.delriogarcia@usp.ceu.es>
2024-10-23 14:08:46 +0200

    pagina registro perro creada

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.get("/perfil", (req, res) => {
 });
 
 app.get("/edit", (req, res) => res.render('editar_perfil'));
+app.get("/perro", (req, res) => res.render('form_perro'));
 app.get("/sesion_cerrada", (req, res) => res.render('sesion_cerrada'));
 
 // Nuevas rutas POST para manejo de formularios
