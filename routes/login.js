f37a00f7901983521e8ffecffe37d0cb67a09e13
DaniFL <danielfernandezlopez75@gmail.com>
2024-11-21 13:17:47 +0100

    Nueva Estructura - v0.15.4 Corrección Login (mensajes session + datos session usuario)

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const User = require("../db/tables/users");
 const router = express.Router();
+const { createUser, getUserByUsername } = require("../db/tables/users");
 //const jwt = require("jsonwebtoken");
 
 /* GET login page. */
@@ -14,7 +14,7 @@ router.get("/", function (req, res, next) {
     navbar_item1: "Home",
     navbar_item2: "Login",
     navbar_item3: "Register",
-    script: "/js/post_login.js",
+    script: "",
     user: req.session.user,
   });
 });
@@ -24,24 +24,34 @@ router.post("/", async function (req, res, next) {
   const { username, password } = req.body;
 
   try {
-    const user = await User.getUserByUsername(username);
-    if (!user) {
-      return res.status(401).json({ message: 'Usuario no encontrado' });
+    const userinput = await getUserByUsername(username);
+    if (!userinput) {
+      req.session.error = "Usuario incorrecto.";
+      console.log("Usuario incorrecto");
+      return res.redirect("/login");
     }
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Contraseña incorrecta' });
+    const passwordMatch = await bcrypt.compare(password, userinput.password);
+    if (!passwordMatch) {
+      req.session.error = "Contraseña incorrecta.";
+      console.log("Contraseña incorrecta");
+      return res.redirect("/login");
     }
 
-    // En caso exitoso redirige a la página de perfil
-    req.session.user = user;
-    //res.status(200).json({ message: 'Inicio de sesión exitoso' });
+    // Crear sesión de usuario
+    req.session.user = {
+      id: userinput.id,
+      username: userinput.username,
+      email: userinput.email,
+    };
+
+    console.log("Inicio de sesión exitoso");
     res.redirect("/profile");
-    
-  } catch (error) {
-    console.error('Error en el inicio de sesión:', error);
-    res.status(500).json({ message: 'Error en el servidor' });
+    console.log(req.session.user);
+  } catch (err) {
+    console.error("Error durante el inicio de sesión:", err);
+    req.session.error = "Hubo un problema al iniciar sesión. Intente de nuevo.";
+    res.redirect("/login");
   }
 });
 
