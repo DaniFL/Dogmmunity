d04a4942133adeb9b8f797a33c573ebff757192f
DaniFL <danielfernandezlopez75@gmail.com>
2024-11-23 14:04:13 +0100

    Redifned routers Get

--- routes/register.js
+++ routes/register.js
@@ -8,11 +8,18 @@ router.get('/', function(req, res, next) {
     res.render("register", {
       title: "Register",
       navbar_addr1: "/",
-      navbar_addr2: "/login",
-      navbar_addr3: "/register",
-      navbar_item1: "Home",
-      navbar_item2: "Login",
-      navbar_item3: "Register",
+    navbar_addr2: "/about_us",
+    navbar_addr3: "/login",
+    navbar_addr4: "/register",
+    navbar_addr5: "",
+    navbar_addr6: "",
+
+    navbar_item1: "Home",
+    navbar_item2: "About Us",
+    navbar_item3: "Login",
+    navbar_item4: "Register",
+    navbar_item5: "",
+    navbar_item6: "Contact",
       script: "",
       user: req.session.user,
     });
